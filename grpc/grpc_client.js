10447f2f4f30e10bfd06a24e80368dac4cefef91
guhdias <guhdias@me.com>
2018-07-02 11:49:14 -0300

    detalhes

--- grpc/grpc_client.js
+++ grpc/grpc_client.js
@@ -3,30 +3,30 @@ var PROTO_PATH = __dirname + '/ep.proto';
 var grpc = require('grpc');
 var ep_proto = grpc.load(PROTO_PATH).ep;
 
-var client = new ep_proto.Teste('localhost:50051',grpc.credentials.createInsecure());
+var client = new ep_proto.Teste('localhost:50051',grpc.credentials.createInsecure()); // se quiser testar com servidor em outra maquina, precisa colocar o ip do servidor aqui
   
 var numTeste = 0;
-var totalTestes = 100;
+var totalTestes = 10;
 var tempos = [];
 
 function main() {
 
-  teste0();
   teste1();
   teste2();
+  teste3();
   for (var i = 0; i < 11; i++) {
-    teste3(Math.pow(2,i), i);
+    teste4(Math.pow(2,i), i);
   }
-  teste4();
+  teste5();
 }
 
 
 /**
- * Teste 0 - 1 argumento e 1 retorno inteiros iguais a 0
+ * Teste 1 - 1 argumento e 1 retorno inteiros iguais a 0
 */
-function teste0() {
+function teste1() {
 // primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
-  client.Teste0({valorRequestInt: 0}, function(err, response) {
+  client.Teste1({valorRequestInt: 0}, function(err, response) {
     if (err) {console.log(err)};
   });
 
@@ -34,7 +34,7 @@ function teste0() {
 
   for (numTeste = 0; numTeste < totalTestes; numTeste++) {
     var tempoInicio = process.hrtime(); // registra tempo de inicio
-    client.Teste0({valorRequestInt: 0}, function(err, response) {
+    client.Teste1({valorRequestInt: 0}, function(err, response) {
     if (err) {console.log(err)};
     //console.log(response);
     });
@@ -43,7 +43,7 @@ function teste0() {
   }
 
 // cálculo e exibicao das estatisticas
-  console.log("------- TESTE 0: 1 argumento e 1 retorno inteiros iguais a 0 -------");
+  console.log("------- TESTE 1: 1 argumento e 1 retorno inteiros iguais a 0 -------");
   console.log("Execucoes:", totalTestes);
   console.log('Media:', media(tempos));
   console.log('Desvio Padrao:', desvioPadrao(tempos));
@@ -52,42 +52,7 @@ function teste0() {
 }
 
 /**
- * Teste 1 - 1 argumento e 1 retorno inteiros aleatorios
-*/
-
-function teste1(){
-// parametros para geracao dos numeros aleatorios
-  var min =  -2147483647;
-  var max = 2147483647;
-
-// primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
-  client.Teste1({valorRequestInt: gerarAleatorio(min,max)}, function(err, response) {
-    if (err) {console.log(err)};
-  });
-
-// realizacao dos testes na quantidade de vezes determinada e armazenamento dos tempos no array
-
-  for (numTeste = 0; numTeste < totalTestes; numTeste++) {
-    var tempoInicio = process.hrtime(); // registra tempo de inicio
-    client.Teste1({valorRequestInt: gerarAleatorio(min,max)}, function(err, response) {
-    if (err) {console.log(err)};
-    //console.log(response);
-    });
-    var tempoFim = process.hrtime(tempoInicio);
-    tempos[numTeste] = ((tempoFim[0]*1000) + (tempoFim[1]/1000000)); // converter o tempo para milisegundos
-  }
-
-// cálculo e exibicao das estatisticas
-  console.log("------- TESTE 1: 1 argumento e 1 retorno inteiros aleatorios -------");
-  console.log("Execucoes:", totalTestes);
-  console.log('Media:', media(tempos));
-  console.log('Desvio Padrao:', desvioPadrao(tempos));
-  console.log('Maxima:', Math.max.apply(null, tempos));
-  console.log('Minima:', Math.min.apply(null, tempos));
-}
-
-/**
- * Teste 2 - 8 argumentos e 8 retornos inteiros aleatorios
+ * Teste 2 - 1 argumento e 1 retorno inteiros aleatorios
 */
 
 function teste2(){
@@ -96,7 +61,7 @@ function teste2(){
   var max = 2147483647;
 
 // primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
-  client.Teste2({valorRequestInt0: gerarAleatorio(min,max), valorRequestInt1: gerarAleatorio(min,max), valorRequestInt2: gerarAleatorio(min,max), valorRequestInt3: gerarAleatorio(min,max), valorRequestInt4: gerarAleatorio(min,max), valorRequestInt5: gerarAleatorio(min,max), valorRequestInt6: gerarAleatorio(min,max), valorRequestInt7: gerarAleatorio(min,max)}, function(err, response) {
+  client.Teste2({valorRequestInt: gerarAleatorio(min,max)}, function(err, response) {
     if (err) {console.log(err)};
   });
 
@@ -104,7 +69,7 @@ function teste2(){
 
   for (numTeste = 0; numTeste < totalTestes; numTeste++) {
     var tempoInicio = process.hrtime(); // registra tempo de inicio
-    client.Teste2({valorRequestInt0: gerarAleatorio(min,max), valorRequestInt1: gerarAleatorio(min,max), valorRequestInt2: gerarAleatorio(min,max), valorRequestInt3: gerarAleatorio(min,max), valorRequestInt4: gerarAleatorio(min,max), valorRequestInt5: gerarAleatorio(min,max), valorRequestInt6: gerarAleatorio(min,max), valorRequestInt7: gerarAleatorio(min,max)}, function(err, response) {
+    client.Teste2({valorRequestInt: gerarAleatorio(min,max)}, function(err, response) {
     if (err) {console.log(err)};
     //console.log(response);
     });
@@ -113,7 +78,7 @@ function teste2(){
   }
 
 // cálculo e exibicao das estatisticas
-  console.log("------- TESTE 2: 8 argumentos e 8 retornos inteiros aleatorios -------");
+  console.log("------- TESTE 2: 1 argumento e 1 retorno inteiros aleatorios -------");
   console.log("Execucoes:", totalTestes);
   console.log('Media:', media(tempos));
   console.log('Desvio Padrao:', desvioPadrao(tempos));
@@ -122,12 +87,16 @@ function teste2(){
 }
 
 /**
- * Teste 3.x - argumento e retorno strings tamanho n (de 1 a 1024)
+ * Teste 3 - 8 argumentos e 8 retornos inteiros aleatorios
 */
 
-function teste3(tamanho, teste){
+function teste3(){
+// parametros para geracao dos numeros aleatorios
+  var min =  -2147483647;
+  var max = 2147483647;
+
 // primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
-  client.Teste3({valorRequestString: stringAleatoria(tamanho), tamanho: tamanho}, function(err, response) {
+  client.Teste3({valorRequestInt0: gerarAleatorio(min,max), valorRequestInt1: gerarAleatorio(min,max), valorRequestInt2: gerarAleatorio(min,max), valorRequestInt3: gerarAleatorio(min,max), valorRequestInt4: gerarAleatorio(min,max), valorRequestInt5: gerarAleatorio(min,max), valorRequestInt6: gerarAleatorio(min,max), valorRequestInt7: gerarAleatorio(min,max)}, function(err, response) {
     if (err) {console.log(err)};
   });
 
@@ -135,7 +104,7 @@ function teste3(tamanho, teste){
 
   for (numTeste = 0; numTeste < totalTestes; numTeste++) {
     var tempoInicio = process.hrtime(); // registra tempo de inicio
-    client.Teste3({valorRequestString: stringAleatoria(tamanho), tamanho: tamanho}, function(err, response) {
+    client.Teste3({valorRequestInt0: gerarAleatorio(min,max), valorRequestInt1: gerarAleatorio(min,max), valorRequestInt2: gerarAleatorio(min,max), valorRequestInt3: gerarAleatorio(min,max), valorRequestInt4: gerarAleatorio(min,max), valorRequestInt5: gerarAleatorio(min,max), valorRequestInt6: gerarAleatorio(min,max), valorRequestInt7: gerarAleatorio(min,max)}, function(err, response) {
     if (err) {console.log(err)};
     //console.log(response);
     });
@@ -144,7 +113,7 @@ function teste3(tamanho, teste){
   }
 
 // cálculo e exibicao das estatisticas
-  console.log("------- TESTE 3." + teste + ": 1 argumento e 1 retorno strings tamanhos " + tamanho + " -------");
+  console.log("------- TESTE 3: 8 argumentos e 8 retornos inteiros aleatorios -------");
   console.log("Execucoes:", totalTestes);
   console.log('Media:', media(tempos));
   console.log('Desvio Padrao:', desvioPadrao(tempos));
@@ -153,10 +122,41 @@ function teste3(tamanho, teste){
 }
 
 /**
- * Teste 4 - argumento e retorno complexos (definidos pelo usuario)
+ * Teste 4.x - argumento e retorno strings tamanho n (de 1 a 1024)
 */
 
-function teste4(){
+function teste4(tamanho, teste){
+// primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
+  client.Teste4({valorRequestString: stringAleatoria(tamanho), tamanho: tamanho}, function(err, response) {
+    if (err) {console.log(err)};
+  });
+
+// realizacao dos testes na quantidade de vezes determinada e armazenamento dos tempos no array
+
+  for (numTeste = 0; numTeste < totalTestes; numTeste++) {
+    var tempoInicio = process.hrtime(); // registra tempo de inicio
+    client.Teste4({valorRequestString: stringAleatoria(tamanho), tamanho: tamanho}, function(err, response) {
+    if (err) {console.log(err)};
+    //console.log(response);
+    });
+    var tempoFim = process.hrtime(tempoInicio);
+    tempos[numTeste] = ((tempoFim[0]*1000) + (tempoFim[1]/1000000)); // converter o tempo para milisegundos
+  }
+
+// cálculo e exibicao das estatisticas
+  console.log("------- TESTE 4." + teste + ": 1 argumento e 1 retorno strings tamanhos " + tamanho + " -------");
+  console.log("Execucoes:", totalTestes);
+  console.log('Media:', media(tempos));
+  console.log('Desvio Padrao:', desvioPadrao(tempos));
+  console.log('Maxima:', Math.max.apply(null, tempos));
+  console.log('Minima:', Math.min.apply(null, tempos));
+}
+
+/**
+ * Teste 5 - argumento e retorno complexos (definidos pelo usuario)
+*/
+
+function teste5(){
 // parametros para geracao dos numeros aleatorios
   var min =  -2147483647;
   var max = 2147483647;
@@ -169,7 +169,7 @@ function teste4(){
   };
 
 // primeira chamada nao eh considerada no teste pois o tempo eh um valor extremo em comparacao as demais
-  client.Teste4(coordenadas, function(err, response) {
+  client.Teste5(coordenadas, function(err, response) {
     if (err) {console.log(err)};
   });
 
@@ -180,7 +180,7 @@ function teste4(){
     coordenadas.x = gerarAleatorio(min,max);
     coordenadas.y = gerarAleatorio(min,max);
     coordenadas.z = gerarAleatorio(min,max);
-    client.Teste4(coordenadas, function(err, response) {
+    client.Teste5(coordenadas, function(err, response) {
     if (err) {console.log(err)};
     //console.log(response);
     });
@@ -189,7 +189,7 @@ function teste4(){
   }
 
 // cálculo e exibicao das estatisticas
-  console.log("------- TESTE 4: argumento e retorno complexos -------");
+  console.log("------- TESTE 5: argumento e retorno complexos -------");
   console.log("Execucoes:", totalTestes);
   console.log('Media:', media(tempos));
   console.log('Desvio Padrao:', desvioPadrao(tempos));
--- grpc/grpc_server.js
+++ grpc/grpc_server.js
@@ -3,32 +3,35 @@ var PROTO_PATH = __dirname + '/ep.proto';
 var grpc = require('grpc');
 var ep_proto = grpc.load(PROTO_PATH).ep;
 
-// Implementacao do metodo
-function Teste0(call, callback) {
-  //console.log(call.request);
-  callback(null, {valorReplyInt: 0});
-}
+/**
+ * funcoes que retornam o resultado em cada teste
+*/
 
 function Teste1(call, callback) {
   //console.log(call.request);
-  var min =  -2147483647;
-  var max = 2147483647;
-  callback(null, {valorReplyInt: gerarAleatorio(min,max)});
+  callback(null, {valorReplyInt: 0});
 }
 
 function Teste2(call, callback) {
   //console.log(call.request);
   var min =  -2147483647;
   var max = 2147483647;
-  callback(null, {valorReplyInt0: gerarAleatorio(min,max), valorReplyInt1: gerarAleatorio(min,max), valorReplyInt2: gerarAleatorio(min,max), valorReplyInt3: gerarAleatorio(min,max), valorReplyInt4: gerarAleatorio(min,max), valorReplyInt5: gerarAleatorio(min,max), valorReplyInt6: gerarAleatorio(min,max), valorReplyInt7: gerarAleatorio(min,max)});
+  callback(null, {valorReplyInt: gerarAleatorio(min,max)});
 }
 
 function Teste3(call, callback) {
   //console.log(call.request);
-  callback(null, {valorReplyString: stringAleatoria(call.request.tamanho), tamanho: call.request.tamanho});
+  var min =  -2147483647;
+  var max = 2147483647;
+  callback(null, {valorReplyInt0: gerarAleatorio(min,max), valorReplyInt1: gerarAleatorio(min,max), valorReplyInt2: gerarAleatorio(min,max), valorReplyInt3: gerarAleatorio(min,max), valorReplyInt4: gerarAleatorio(min,max), valorReplyInt5: gerarAleatorio(min,max), valorReplyInt6: gerarAleatorio(min,max), valorReplyInt7: gerarAleatorio(min,max)});
 }
 
 function Teste4(call, callback) {
+  //console.log(call.request);
+  callback(null, {valorReplyString: stringAleatoria(call.request.tamanho), tamanho: call.request.tamanho});
+}
+
+function Teste5(call, callback) {
   //console.log(call.request);
   var objeto = {
     nome: stringAleatoria(64),
@@ -42,10 +45,15 @@ function Teste4(call, callback) {
 */
 function main() {
   var server = new grpc.Server();
-  server.addService(ep_proto.Teste.service, {Teste0: Teste0, Teste1: Teste1, Teste2: Teste2, Teste3: Teste3, Teste4: Teste4});
+  server.addService(ep_proto.Teste.service, {Teste1: Teste1, Teste2: Teste2, Teste3: Teste3, Teste4: Teste4, Teste5: Teste5});
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
+  console.log("Servidor em execucao. Para interromper utilize Ctrl+C.");
 }
+
+/**
+ * Funcoes de apoio
+*/
 
 function gerarAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
